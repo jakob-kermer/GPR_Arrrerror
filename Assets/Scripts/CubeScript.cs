4603e149e1fa906ac99c8090e9f66bb3ae83626a
fhb251231 <fhb251231@fh-vie.ac.at>
2025-12-22 15:48:59 +0100

    Comment on Cube Script
    Test change

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -5,7 +5,9 @@ public class CubeScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Sends a Hello World to the user
         Debug.Log("Hello, World!");
+
     }
 
     // Update is called once per frame
